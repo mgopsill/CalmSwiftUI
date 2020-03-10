41ba9e6e9107794d07f44599fc251994d010c212
Mike Gopsill <m.gopsill@gmail.com>
2020-03-10 15:28:06 +0000

    Initial Commit

---
+++ CalmSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CalmSwiftUI
+//
+//  Created by Mike Gopsill on 10/03/2020.
+//  Copyright © 2020 mgopsill. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
