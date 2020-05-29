da967364f20d18bac1bb9c5693afa252fc16869f
Mike Gopsill <m.gopsill@gmail.com>
2020-05-29 06:48:54 +0100

    refactor: playing with a more combiney way of caching images

--- CalmSwiftUI/ImageCache.swift
+++ CalmSwiftUI/ImageCache.swift
@@ -6,6 +6,7 @@
 //  Copyright © 2020 mgopsill. All rights reserved.
 //
 
+import Combine
 import SwiftUI
 
 final class ImageCache {
@@ -22,5 +23,32 @@ final class ImageCache {
 
     func loadImage(for key: URL) -> Image? {
         return imageCache.object(forKey: key as AnyObject) as? Image
+    }
+}
+
+final class ReactiveImageCache {
+    private init() { }
+    static let shared: ReactiveImageCache = ReactiveImageCache()
+
+    private let imageCache = NSCache<AnyObject, AnyObject>()
+
+    func cache(_ image: Image, for key: URL) {
+        let imageToCache = image as AnyObject
+        let key = key as AnyObject
+        imageCache.setObject(imageToCache, forKey: key)
+    }
+    
+    func loadImage(for key: URL) -> Future<Image, Error> {
+        return Future<Image, Error> { promise in
+            if let image = self.imageCache.object(forKey: key as AnyObject) as? Image {
+                promise(.success(image))
+            } else {
+                promise(.failure(ImageCacheError.noCachedImage))
+            }
+        }
+    }
+    
+    enum ImageCacheError: Error {
+        case noCachedImage
     }
 }
--- CalmSwiftUI/ImageLoader.swift
+++ CalmSwiftUI/ImageLoader.swift
@@ -12,30 +12,39 @@ import SwiftUI
 final class ImageLoader: ObservableObject {
     @Published var image: Image?
     
-    var cancellables: Set<AnyCancellable>? = []
-    
-    deinit {
-        cancellables?.forEach { $0.cancel() }
-    }
+    private var cancellables: Set<AnyCancellable> = []
     
     init(url: URL) {
-        if let image = Current.cache.loadImage(url) {
-            self.image = image
-        } else {
-            let fetchImage = URLSession.shared.dataTaskPublisher(for: url)
-                .map { $0.data }
-                .compactMap { UIImage(data: $0) }
-                .map { Image(uiImage: $0) }
-                .receive(on: RunLoop.main)
-                .replaceError(with: nil)
-                .eraseToAnyPublisher()
-                .assign(to: \.image, on: self)
-            
-            let cacheImage = $image.compactMap { $0 }
-                .sink { Current.cache.cacheImageForURL($0,url) }
-            
-            cancellables?.insert(fetchImage)
-            cancellables?.insert(cacheImage)
-        }
+        // Create a url session publisher that will fetch data
+        let fetchImageFromNetwork = URLSession.shared.dataTaskPublisher(for: url)
+            .map { $0.data }
+            .compactMap { UIImage(data: $0) }
+            .map { Image(uiImage: $0) }
+            .receive(on: RunLoop.main)
+            .replaceError(with: nil)
+            .eraseToAnyPublisher()
+            .compactMap { $0 }
+            .multicast { PassthroughSubject<Image, Never>() }
+        // ☝️ multicast creates a subject, allowing you to subscribe multiple times to one stream
+        // call .connect() on the original stream to start the stream
+        
+        // below two subcribes, one sets the image and another caches the image
+        fetchImageFromNetwork
+            .sink { self.image = $0 }
+            .store(in: &cancellables)
+        fetchImageFromNetwork
+            .sink { ReactiveImageCache.shared.cache($0, for: url) }
+            .store(in: &cancellables)
+
+        // this loads the image
+        ReactiveImageCache.shared.loadImage(for: url)
+            .sink(receiveCompletion: { _ in
+                // if loading image from cache fails we come here and start network
+                fetchImageFromNetwork.connect().store(in: &self.cancellables)
+            }, receiveValue: { image in
+                // if we get an image we simply set it
+                self.image = image
+            })
+            .store(in: &cancellables)
     }
 }
